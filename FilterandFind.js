172db9340c2f8a216285919ea4dac565808e2d5a
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-04 10:45:57 +0100

    additional classes

---
+++ FilterandFind.js
@@ -0,0 +1,6 @@
+const people = [
+    { name : 'bob', age : 20, position : 'developer'},
+    { name : 'peter', age : 25, position : 'designer'},
+    { name : 'susy', age : 30, position : 'the boss'},
+    { name : 'anna', age : 35, position : 'intern'},
+]

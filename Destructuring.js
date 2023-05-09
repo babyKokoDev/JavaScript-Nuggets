2bbe4e024e81fb2555519a1e31e0fb452992e8b3
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-09 08:47:29 +0100

    additional class

--- Destructuring.js
+++ Destructuring.js
@@ -1,6 +1,6 @@
 const fruits = ['orange', 'banana', 'lemon']
 const friends = ['john', 'peter', 'bob', 'anna', 'kelly']
 
-const [a, b, c, d, e] = friends
+const [a, , , , e] = friends
 
-console.log(a,b,c,d,e)
+console.log(a, e)

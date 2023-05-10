f27c0980772602b09d73362c33ebcb2dbe729e81
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-10 09:44:15 +0100

    additional class

--- RestOperator.js
+++ RestOperator.js
@@ -1,5 +1,6 @@
 const fruits = ['apple', 'orange', 'lemon', 'banana']
-const [first] = fruits
+const [first, ...rest] = fruits
+console.log(first, rest)
 
 const person = {name : 'john', lastName : 'smith', job : 'developer'}
 

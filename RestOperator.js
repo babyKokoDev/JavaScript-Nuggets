ea72e614247ea93f3da315108d3cf1ad6ee8cfaa
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-23 12:31:53 +0100

    additional class

--- RestOperator.js
+++ RestOperator.js
@@ -1,8 +1,20 @@
 const fruits = ['apple', 'orange', 'lemon', 'banana']
-const [first, ...rest] = fruits
-console.log(first, rest)
-
+const [first, ...restOfFruits] = fruits
+console.log(first, restOfFruits)
+const specificFruits = restOfFruits.find((fruit)=>fruit === "lemon")
+console.log(specificFruits)
 const person = {name : 'john', lastName : 'smith', job : 'developer'}
 
-const {name} = person
-console.log(name)
+const {name, ...rest} = person
+console.log(name, rest)
+
+const getAverage = (name, ...scores) => {
+    console.log(name)
+    const average = scores.reduce((total, item)=>{
+        total += item/scores.length
+        return total
+    }, 0)
+    console.log(average)
+}
+const total = [89,76,81,100]
+getAverage("Bimpe", ...total)

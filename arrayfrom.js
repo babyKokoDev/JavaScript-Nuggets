bcb2b94e8496e4c98dccd14b7e9e447ac15229e9
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-24 09:59:16 +0100

    additional class

---
+++ arrayfrom.js
@@ -0,0 +1,17 @@
+const udemy = 'udemy'
+// console.log(Array.from(udemy));
+
+const items = Array.from({length: 120}, (_, index)=>{
+  return index
+})
+
+const itemsPerPage = 14
+const pages = Math.ceil(items.length / itemsPerPage)
+
+const newItems = Array.from({length:pages}, (_, index)=>{
+    const start = index * itemsPerPage
+    const tempItems = items.slice(start, start + itemsPerPage)
+    return tempItems
+})
+console.log(newItems);
+

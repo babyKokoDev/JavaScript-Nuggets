86d717bad4a3e6575a7da7a6e13f0e7dc46f47bb
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-26 08:40:53 +0100

    additional classes

--- arrayfrom.js
+++ arrayfrom.js
@@ -1,17 +1,31 @@
 const udemy = 'udemy'
 // console.log(Array.from(udemy));
 
-const items = Array.from({length: 120}, (_, index)=>{
-  return index
+// const items = Array.from({length: 120}, (_, index)=>{
+//   return index
+// })
+
+// const itemsPerPage = 14
+// const pages = Math.ceil(items.length / itemsPerPage)
+
+// const newItems = Array.from({length:pages}, (_, index)=>{
+//     const start = index * itemsPerPage
+//     const tempItems = items.slice(start, start + itemsPerPage)
+//     return tempItems
+// })
+// console.log(newItems);
+
+
+const items = Array.from({length : 120}, (_, index)=>{
+    return index
+})
+ const itemsPerPage = 10
+ const page = items.length / itemsPerPage
+
+const newItems = Array.from({length : page}, (_, index)=>{
+   const start = index * itemsPerPage
+   const newArray = items.slice(start, start + itemsPerPage)
+   return newArray
 })
 
-const itemsPerPage = 14
-const pages = Math.ceil(items.length / itemsPerPage)
-
-const newItems = Array.from({length:pages}, (_, index)=>{
-    const start = index * itemsPerPage
-    const tempItems = items.slice(start, start + itemsPerPage)
-    return tempItems
-})
-console.log(newItems);
-
+console.log(newItems)
